032013c1e09d2e9218d4751ca4d94df568bbdc4a
Mika Sormunen <mika.sormunen@citrus.fi>
2019-10-06 22:45:41 +0300

    Initial project

--- src/config.js
+++ src/config.js
@@ -3,11 +3,23 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '<you should create your own api key>';
+const API_KEY = 'cb4680a73f2c7daff83e0c1c18510fbe';
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=`
+const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}`
+
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280';
 // w92, w154, w185, w342, w500, w780, original
 const POSTER_SIZE = 'w500';
 
-export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
+export {
+    API_URL,
+    API_KEY,
+    IMAGE_BASE_URL,
+    SEARCH_BASE_URL,
+    POPULAR_BASE_URL,
+    BACKDROP_SIZE,
+    POSTER_SIZE
+};
